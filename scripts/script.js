983b897be691cdbcb94e7bc30563aac729705cf0
JOSSIE_YAN <jiayin.yan.jossie@gmail.com>
2025-03-03 23:05:52 -0800

    Add event listeners for DOM elements to display console messages

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ if (newSessionButton) { // check to see if button exists in that page
       });
 }
 
-// settingsButton profile.html
+// settingsButton profile.html and navbars
 function goSettings() {
       console.log('You went to the settings!'); // in future sprints, these will do something
 }
@@ -36,3 +36,59 @@ if (logoutButton) {
             logOut();
       });
 }
+
+
+// task in navbar
+function showTaskList() {
+      console.log('Here is your task list!'); // in future sprints, these will do something
+}
+
+const taskListNav = document.getElementById('taskListNav');
+if (taskListNav) {
+      taskListNav.addEventListener('click', function () {
+            showTaskList();
+      });
+}
+
+
+// timer in nav
+function getTimer() {
+      console.log('Here is a timer for your studies'); // in future sprints, these will do something
+}
+
+const timerNav = document.getElementById('timerNav');
+if (timerNav) {
+      timerNav.addEventListener('click', function () {
+            getTimer();
+      });
+}
+
+
+
+//  user stats nav
+function showUserStats() {
+      console.log('Here are your stats! Wow good job!'); // in future sprints, these will do something
+}
+
+const userStatsNav = document.getElementById('userStatsNav');
+if (userStatsNav) {
+      userStatsNav.addEventListener('click', function () {
+            showUserStats();
+      });
+}
+
+
+//  leaderboard
+function showLeaderBoard() {
+      console.log('Show leaderboard'); // in future sprints, these will do something
+}
+
+const leaderboardNav = document.getElementById('leaderboardNav');
+if (leaderboardNav) {
+      leaderboardNav.addEventListener('click', function () {
+            showLeaderBoard();
+      });
+}
+
+
+
