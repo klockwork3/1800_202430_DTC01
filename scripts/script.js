6275882b51cc64475d3898ad7216477ceec5ec6b
JOSSIE_YAN <jiayin.yan.jossie@gmail.com>
2025-03-03 21:57:50 -0800

    Implement console messages for eventlisteners

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,33 @@
 
-function sayHello() {
+// newSessionButton main.html
+function newSession() {
       console.log('Hello! The button was clicked!'); // in future sprints, these will do something
 }
 
 document.getElementById('newSessionButton').addEventListener('click', function (event) {
       event.preventDefault(); // Prevent the default link behavior
-      sayHello(); // Call the sayHello function
+      newSession();
 });
+
+
+// settingsButton profile.html
+function goSettings() {
+      console.log('You went to the settings!'); // in future sprints, these will do something
+}
+
+document.getElementById('settingsButton').addEventListener('click', function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      goSettings();
+});
+
+
+// logoutButton profile.html
+function logOut() {
+      console.log('You have logged out!'); // in future sprints, these will do something
+}
+
+document.getElementById('logoutButton').addEventListener('click', function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      logOut();
+});
+
