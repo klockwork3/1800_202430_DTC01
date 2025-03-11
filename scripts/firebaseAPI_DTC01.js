f7752dbc3d61dd36f50625880f2bf931e3be45b4
JOSSIE_YAN <jiayin.yan.jossie@gmail.com>
2025-03-10 22:08:20 -0700

    Remove firebase key...

--- scripts/firebaseAPI_DTC01.js
+++ scripts/firebaseAPI_DTC01.js
@@ -3,12 +3,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: "AIzaSyDgPGjGouo9_f7uepszHldondUmQ24_c98",
-    authDomain: "comp-1800-project-6eb78.firebaseapp.com",
-    projectId: "comp-1800-project-6eb78",
-    storageBucket: "comp-1800-project-6eb78.firebasestorage.app",
-    messagingSenderId: "498376410852",
-    appId: "1:498376410852:web:96a950d91b4ac2ede44ef1"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 //--------------------------------------------
